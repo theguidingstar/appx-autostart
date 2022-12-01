fd5fca651bc32e6e769d3c092510c3cb9b6e4932
Yash Shah <yashshh123@gmail.com>
2022-11-30 23:27:57 -0800

    Updated UI for autostart

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,35 +5,15 @@ import './App.css';
 const Hello = () => {
   return (
     <div>
+      <h1>AppX Auto Start</h1>
+      <br/>
       <div className="Hello">
-        <img width="200" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
           <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
+            Enable Auto Start
+          </button> &nbsp;
           <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
+            Disable Autostart
           </button>
-        </a>
       </div>
     </div>
   );
